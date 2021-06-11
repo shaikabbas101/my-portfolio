f1f2c1e8854be1a735f0a714cc39d721a9357dcc
Shaik Abbas <shaikabbas101@gmail.com>
2021-06-11 18:59:19 +0530

    capitalizing name

--- src/components/HomeSection/index.js
+++ src/components/HomeSection/index.js
@@ -2,7 +2,7 @@ import './index.css'
 
 const HomeSection = () => (
   <div className="home-section">
-    <h1 className="username">Shaik Abbas</h1>
+    <h1 className="username">SHAIK ABBAS</h1>
     <p className="designation">Full Stack Developer</p>
     <p className="experience-description">
       Experienced in MongoDB, Express, ReactJS and Node.js, Bootstrap, HTML,
