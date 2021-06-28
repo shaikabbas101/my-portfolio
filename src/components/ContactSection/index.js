90f8047c8714814c5c96f7633fcde204a040de01
Shaik Abbas <shaikabbas101@gmail.com>
2021-06-28 23:10:28 +0530

    location img change

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -5,7 +5,7 @@ const ContactSection = () => (
     <h1 className="contact-section-heading">My Contact</h1>
     <img
       className="location-img"
-      src="https://assets.ccbp.in/frontend/react-js/location-img.png"
+      src="https://res.cloudinary.com/dkr26vkii/image/upload/v1624901425/2021-06-28_2_uexsip.png"
       alt="location-map"
     />
     <p className="contact-description">
